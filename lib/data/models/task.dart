b282c5a37ad8f477a815bdeac71ecdc54904443a
alsayeed <alxayeed@gmail.com>
2023-03-15 20:20:23 +0600

    Update: dependecy added. model and ui created

---
+++ lib/data/models/task.dart
@@ -0,0 +1,52 @@
+import 'package:equatable/equatable.dart';
+
+class Task extends Equatable {
+  final String id;
+  final String title;
+  bool? isCompleted;
+  bool? isDeleted;
+
+  Task({
+    required this.id,
+    required this.title,
+    this.isCompleted,
+    this.isDeleted,
+  }) {
+    isCompleted = isCompleted ?? false;
+    isDeleted = isDeleted ?? false;
+  }
+
+  Task copyWith({
+    String? id,
+    String? title,
+    bool? isCompleted,
+    bool? isDeleted,
+  }) {
+    return Task(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? false,
+      isDeleted: isDeleted ?? false,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'isCompleted': isCompleted,
+      'isDeleted': isDeleted
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+        id: map["id"] ?? '',
+        title: map["title"] ?? '',
+        isCompleted: map["isCompleted"] ?? '',
+        isDeleted: map["isDeleted"] ?? '');
+  }
+
+  @override
+  List<Object?> get props => [id, title, isCompleted, isDeleted];
+}
