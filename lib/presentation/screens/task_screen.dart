36b8d516cb8827e81c08447f6bbea68f8864de49
alsayeed <alxayeed@gmail.com>
2023-03-15 20:29:30 +0600

    Update: dummy task list added to the ui

--- lib/presentation/screens/task_screen.dart
+++ lib/presentation/screens/task_screen.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../../data/models/task.dart';
+
 class TaskScreen extends StatelessWidget {
-  const TaskScreen({Key? key}) : super(key: key);
+  TaskScreen({Key? key}) : super(key: key);
+
+  final List<Task> allTask = [
+    Task(id: "id1", title: "Task 1"),
+    Task(id: "id2", title: "Task 2"),
+    Task(id: "id3", title: "Task 3"),
+    Task(id: "id4", title: "Task 4"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +25,25 @@ class TaskScreen extends StatelessWidget {
           ],
         ),
         body: Column(
-          children: const [
-            Center(
+          children: [
+            const Center(
               child: Chip(label: Text('Tasks')),
-            )
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: allTask.length,
+                itemBuilder: (context, index) {
+                  Task task = allTask[index];
+                  return ListTile(
+                    title: Text(task.title),
+                    trailing: Checkbox(
+                      value: false,
+                      onChanged: (bool? value) {},
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
         floatingActionButton: FloatingActionButton(
